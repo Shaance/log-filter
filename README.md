a34f31cd5a23d0c625738b3457b995391e953b86
Shaance <christophe.ha1@gmail.com>
2019-06-22 15:00:03 +0200

    add logic

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 #Log filter
 
 Simple program written in Go to filter log file on log level basis - made for training purposes.
-# log-filter
+
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	logLevels := map[string]bool{"INFO": true, "DEBUG": true, "ERROR": true, "CRITICAL": true}
+	path, logLevel := getProgramArguments()
+
+	if !logLevels[*logLevel] {
+		log.Fatal(*logLevel + " value not recognized. See the -level option usage to learn more about available options")
+	}
+
+	file, err := os.Open(*path)
+	defer file.Close()
+	terminateOnError(err)
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, *logLevel) {
+			fmt.Println(line)
+		}
+	}
+
+	terminateOnError(scanner.Err())
+
+}
+
+func getProgramArguments() (*string, *string) {
+	path := flag.String("path", "logfile.log", "The path to the log file to be filtered.")
+	logLevel := flag.String("level", "ERROR", "Log level to search for. "+
+		"Options are DEBUG, INFO, ERROR and CRITICAL")
+	flag.Parse()
+
+	return path, logLevel
+}
+
+func terminateOnError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
